d89236c6662209e952efdbb2016260fde509313e
Carlos Paredes <00028516@uca.edu.sv>
2019-12-03 20:24:01 -0600

    first commit

--- src/com/fantasticCode/entities/OfferTestUnit.java
+++ src/com/fantasticCode/entities/OfferTestUnit.java
@@ -2,7 +2,6 @@ package com.fantasticCode.entities;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
